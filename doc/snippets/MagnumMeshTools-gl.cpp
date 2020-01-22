05de3b9a934561361c2bb0a2b47e71fc05943b49
Vladimír Vondruš <mosra@centrum.cz>
2020-01-22 17:01:59 +0100

    MeshTools: deSTLify compressIndices().
    There's a lot to change with the current version -- the bloaty tuple,
    the useless min/max, and compressing all the way down to 8 bits is not
    desirable anymore either. The new function allows to specify a minimal
    type to compress to and works also on 8- and 16-byte types, which makes
    it possible to also inflate a smaller type into a larger one.
    
    The old function is now deprecated.

--- doc/snippets/MagnumMeshTools-gl.cpp
+++ doc/snippets/MagnumMeshTools-gl.cpp
@@ -23,6 +23,8 @@
     DEALINGS IN THE SOFTWARE.
 */
 
+#include <tuple> /* for std::tie() :( */
+
 #include "Magnum/GL/Buffer.h"
 #include "Magnum/GL/Mesh.h"
 #include "Magnum/Math/Vector3.h"
@@ -35,6 +37,25 @@ int main() {
 
 {
 /* [compressIndices] */
+Containers::Array<UnsignedInt> indices;
+
+Containers::Array<char> indexData;
+MeshIndexType indexType;
+std::tie(indexData, indexType) = MeshTools::compressIndices(indices);
+
+GL::Buffer indexBuffer;
+indexBuffer.setData(indexData);
+
+GL::Mesh mesh;
+mesh.setCount(indices.size())
+    .setIndexBuffer(indexBuffer, 0, indexType);
+/* [compressIndices] */
+}
+
+#ifdef MAGNUM_BUILD_DEPRECATED
+{
+CORRADE_IGNORE_DEPRECATED_PUSH
+/* [compressIndices-stl] */
 std::vector<UnsignedInt> indices;
 
 Containers::Array<char> indexData;
@@ -49,8 +70,10 @@ indexBuffer.setData(indexData, GL::BufferUsage::StaticDraw);
 GL::Mesh mesh;
 mesh.setCount(indices.size())
     .setIndexBuffer(indexBuffer, 0, indexType, indexStart, indexEnd);
-/* [compressIndices] */
+/* [compressIndices-stl] */
 }
+CORRADE_IGNORE_DEPRECATED_POP
+#endif
 
 {
 struct MyShader {
